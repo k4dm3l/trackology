c26ed37715e0948b49a9b94e0ae6025f1e08515f
Camilo Acevedo <camilo-mac@Camilos-MacBook-Pro.local>
2021-10-09 22:46:06 -0500

    Added information for request time

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const { getIpGeolocationInformation } = require("./utils/geolocation");
 const { getClientIpRequest } = require("./utils/ip-headers-check");
 
 const getAuditInformation = (request, response, next) => {
-  const ips = getClientIpRequest(request);
-  request['audit'] = {
-    ips
-  };
-  next();
+  const clientIps = getClientIpRequest(request);
+  const requestTime = new Date();
+
+  return new Promise(async (resove, reject) => {
+    await Promise.all(
+      clientIps.map(async (ip) => {
+        ip["requestTime"] = requestTime;
+
+        try {
+          const geolocation = await getIpGeolocationInformation(ip.value);
+          ip["geolocation"] = geolocation;
+        } catch (error) {
+          ip["geolocation"] = {};
+        }
+      })
+    );
+
+    request["audit"] = { clientIps };
+    resove(next());
+  });
 };
 
 module.exports = {
